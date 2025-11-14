72c29ea6ff0524752ed8b7206ae71a3b1679e7f3
echoshade800 <echoshade800@gmail.com>
2025-11-13 23:42:28 -0800

    Updated fishing.js

--- app/fishing.js
+++ app/fishing.js
@@ -1277,7 +1277,7 @@ const styles = StyleSheet.create({
   },
   characterImage: {
     position: 'absolute',
-    bottom: SCREEN_HEIGHT * 0.15 + 50,
+    bottom: SCREEN_HEIGHT * 0.15,
     left: SCREEN_WIDTH * 0.35,
     width: SCREEN_WIDTH * 0.3,
     height: SCREEN_HEIGHT * 0.4,
